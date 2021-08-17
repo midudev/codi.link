6465d3ec378ee92754b7963909d604a7dc769e47
midudev <miduga@gmail.com>
2021-08-17 21:51:09 +0200

    refactor(Root): Remove console.log

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import JsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
-console.log('holaaaaaaaaaa')
-
 window.MonacoEnvironment = {
   getWorker (_, label) {
     if (label === 'html') return new HtmlWorker()
