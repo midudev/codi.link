4debe13f04ef5b00805bb61b1b0d344840cdbfe3
reliut-g <reliutg@gmail.com>
2021-09-05 00:28:32 +0200

    fix: lint env

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,21 +1,21 @@
+/* eslint-env browser */
 export const $ = (target) => {
-    /**
-     * Add "$" method to HTMLElement interface.
-     * Now we can use parentNode.$("target") to find a DOM element.
-     */
-    Object.defineProperty(HTMLElement.prototype, "$", {
-        value: $,
-    });
+  /**
+   * Add "$" method to HTMLElement interface.
+   * Now we can use parentNode.$("target") to find a DOM element.
+   */
+  Object.defineProperty(HTMLElement.prototype, '$', {
+    value: $
+  })
 
-    if (target.length === 0) return undefined;
+  if (target.length === 0) return undefined
 
-    const selected =
-        this instanceof HTMLElement
-            ? this.querySelectorAll(target)
-            : document.querySelectorAll(target);
+  const selected =
+    this instanceof HTMLElement
+      ? this.querySelectorAll(target)
+      : document.querySelectorAll(target)
 
-    const isArr =
-        selected instanceof NodeList && Array.from(selected).length > 1;
+  const isArr = selected instanceof NodeList && Array.from(selected).length > 1
 
-    return isArr ? selected : selected[0];
+  return isArr ? selected : selected[0]
 }
