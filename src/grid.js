7515833f5bc5e94d54b0cfff39d2a411e1df0303
Eliut Gon <reliutg@gmail.com>
2021-09-17 12:10:32 +0200

    refactor(Root): prevent icons from being hidden

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,16 @@
 import Split from 'split-grid'
 import { $ } from './utils/dom.js'
 
+const $aside = $('aside')
+const asideWidth = $aside.offsetWidth
+
+const iconSize = 48
+const columnMinSize = asideWidth + iconSize
+const columnMaxSize = window.innerHeight - asideWidth - iconSize
+
+const rowMinSize = iconSize
+const rowMaxSize = window.innerHeight - iconSize
+
 Split({
   columnGutters: [{
     track: 1,
@@ -9,5 +19,9 @@ Split({
   rowGutters: [{
     track: 1,
     element: $('.horizontal-gutter')
-  }]
+  }],
+  columnMinSize,
+  columnMaxSize,
+  rowMinSize,
+  rowMaxSize
 })
