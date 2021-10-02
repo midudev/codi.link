ac0aeb31d421283b5c952f055c5899aec00a1028
Tomas Melone <tomasmelone@hotmail.com>
2021-10-03 00:50:02 +0200

    feat(Root): Add function to remove id from url

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,3 +2,7 @@ export const getSessionId = () => {
   const urlMatch = window.location.search.match(/\?join=([\w-]*)/)
   return urlMatch ? urlMatch[1] : null
 }
+
+export const removeIdFromUrl = () => {
+  window.history.replaceState(null, null, window.location.pathname)
+}
