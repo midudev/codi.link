443261dadc18404f2bd7a71f6e4631db7d438661
SirReiva <gjjavicoru@hotmail.com>
2021-10-02 23:13:31 +0200

    feat: :sparkles: mutiple resize gutter

--- src/grid.js
+++ src/grid.js
@@ -5,9 +5,15 @@ Split({
   columnGutters: [{
     track: 1,
     element: $('.vertical-gutter')
+  }, {
+    track: 1,
+    element: $('.central-gutter')
   }],
   rowGutters: [{
     track: 1,
     element: $('.horizontal-gutter')
+  }, {
+    track: 1,
+    element: $('.central-gutter')
   }]
 })
