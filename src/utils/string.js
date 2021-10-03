d40981bd691c62c75cfb1b09056a27284d8e6a85
Eliut <63687573+eliutgon@users.noreply.github.com>
2021-10-03 18:06:26 +0200

    feat: function to search for a text per line

--- src/utils/string.js
+++ src/utils/string.js
@@ -4,3 +4,16 @@ export const capitalize = (str) => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join('')
 }
+
+/**
+ * Search for a string line in a string
+ * @param {string} str Search text
+ * @param {string} input Where to search
+ * @param {number} lines In how many lines do we want to search
+ * @returns {string} Line finded
+ */
+export const searchByLine = (str, input, lines = 10) => {
+  const linesArr = str.split('\n')
+  const parsedInput = input.endsWith(';') ? input.slice(0, -1) : input
+  return linesArr.slice(0, lines).find((line) => line.includes(parsedInput))
+}
