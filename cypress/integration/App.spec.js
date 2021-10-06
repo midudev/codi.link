e1cbe838eb5d544610f3790b48944321834779df
Diego <diego.garciabrisa@maibornwolff.de>
2021-10-06 08:50:24 +0200

    feat(cypress): add config and delete App.spec

--- cypress/integration/App.spec.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import { CheckImportSkypack } from './Skypack.spec'
-
-beforeEach(() => {
-  cy.visit('http://localhost:3000/')
-})
-
-CheckImportSkypack()
--- cypress/integration/Skypack.spec.js
+++ cypress/integration/Skypack.spec.js
@@ -1,6 +1,10 @@
+import { beforeEachTest, afterEachTest } from './helper'
 /* eslint-disable no-undef */
-export const CheckImportSkypack = () => {
-  describe('Check Skypack is importing correctly', () => {
+
+const CheckImportSkypack = () => {
+  beforeEachTest()
+  afterEachTest()
+  return describe('Check Skypack is importing correctly', () => {
     it('Import React and check that is imported in the JS', () => {
       cy.get('[data-action="show-skypack-bar"]').click()
       cy.get('#skypack-search-input').clear()
@@ -8,27 +12,27 @@ export const CheckImportSkypack = () => {
       cy.get(
         '[title="React is a JavaScript library for building user interfaces."]'
       ).click()
-      cy.get('[style="top:16px;height:24px;"] > :nth-child(1) > .mtk22').should(
+      return cy.get('[style="top:16px;height:24px;"] > :nth-child(1) > .mtk22').should(
         'have.text',
         'React'
       )
-
-      it('Type @ and get 0 results', () => {
-        cy.get('#skypack-search-input').type('@')
-        cy.get('.skypack-item').click()
-        cy.get('.search-results-message').should(
-          'have.text',
-          '0 results for "@"'
-        )
-      })
-
-      it('Import @Material-ui and check if the @ is imported', () => {
-        cy.get('#skypack-search-input').type('@material')
-        cy.get(':nth-child(1) > small').click()
-        cy.get(
-          '[style="top:16px;height:24px;"] > :nth-child(1) > .mtk22'
-        ).should('have.text', 'MaterialUiCore')
-      })
+    })
+    it('Type @ and get 0 results', () => {
+      cy.get('#skypack-search-input').type('@')
+      cy.get('.skypack-item').click()
+      return cy.get('.search-results-message').should(
+        'have.text',
+        '0 results for "@"'
+      )
+    })
+    it('Import @Material-ui and check if the @ is imported', () => {
+      cy.get('#skypack-search-input').type('@material')
+      cy.get(':nth-child(1) > small').click()
+      return cy.get(
+        '[style="top:16px;height:24px;"] > :nth-child(1) > .mtk22'
+      ).should('have.text', 'MaterialUiCore')
     })
   })
 }
+
+CheckImportSkypack()
