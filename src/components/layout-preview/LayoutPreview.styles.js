a895d948b70709cedb051fab1c4ed0e827587690
Miguel Ángel Durán <miduga@gmail.com>
2021-10-08 13:45:36 +0200

    Add some break lines

--- src/components/layout-preview/LayoutPreview.styles.js
+++ src/components/layout-preview/LayoutPreview.styles.js
@@ -17,31 +17,38 @@ export const LayoutPreviewStyles = css`
   background-color: var(--layout-preview-background-color, #797b80);
   border-radius: var(--layout-preview-border-radius, 5px);
 }
+
 :host([layout=layout-2]){
   grid-template-areas: 'html js' 'css result';
 }
+
 :host([layout=vertical]){
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: 1fr;
   grid-template-areas: 'html css js result';
 }
+
 :host([layout=horizontal]){
   grid-template-rows: repeat(4, 1fr);
   grid-template-columns: 1fr;
   grid-template-areas: 'html' 'css' 'js' 'result';
 }
+
 .html {
   grid-area: html;
   background-color: var(--layout-preview-background-color-html, #e34f26);
 }
+
 .css {
   grid-area: css;
   background-color: var(--layout-preview-background-color-css, #30a9dc);
 }
+
 .js {
   grid-area: js;
   background-color: var(--layout-preview-background-color-js, #f7df1e);
 }
+
 .result {
   grid-area: result;
   background-color: var(--layout-preview-background-color-result, #ffffff);
