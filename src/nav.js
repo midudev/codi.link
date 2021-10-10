f644356b60a405b1da6bfb0a2bea75f601364ce9
Diego Artiles <diego45882@hotmail.com>
2021-10-10 16:28:59 -0300

    feat(Root): persist authentication status

--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,33 @@
 import { PROVIDERS } from './constants/authentication-providers.js'
 import { authenticate } from './services/authentication/index.js'
+import { getState } from './state.js'
 import { $ } from './utils/dom.js'
 
 const $navBar = $('#nav')
 const $navBarRightSection = $('#nav-right-section')
+const { updateSettings, ...settings } = getState()
+const { user } = settings
 
-$navBar.addEventListener('click', async () => {
-  const { data } = await authenticate(PROVIDERS.GITHUB)
-  if (!data) return undefined
-  $navBarRightSection.innerHTML = `<div class="logged-user"><span>${data.user.reloadUserInfo.screenName}</span><img src="${data.user.photoURL}" alt="user-avatar"></div>`
-})
+const setLoggedUserUI = () => {
+  $navBarRightSection.innerHTML = `<div class="logged-user"><span>${user.reloadUserInfo.screenName}</span><img src="${user.photoURL}" alt="user-avatar"></div>`
+}
+
+if (user.token) setLoggedUserUI()
+
+if (!user.token) {
+  $navBar.addEventListener('click', async () => {
+    const { data } = await authenticate(PROVIDERS.GITHUB)
+    if (!data) return undefined
+    setLoggedUserUI()
+
+    const { token, user } = data
+
+    updateSettings({
+      key: 'user',
+      value: {
+        token,
+        ...user
+      }
+    })
+  })
+}
