50021224b43a426e620c070109b6e31a7047b4c8
stylessh <stylesshjs@gmail.com>
2021-10-11 11:56:51 -0400

    refactor: files formatted to remove extra lines, previous layout section removed

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,12 +1,9 @@
-export const $ = (selector, context = document) =>
-  context.querySelector(selector)
+export const $ = (selector, context = document) => context.querySelector(selector)
 
-export const $$ = (selector, context = document) =>
-  context.querySelectorAll(selector)
+export const $$ = (selector, context = document) => context.querySelectorAll(selector)
 
 export const isNodeSelect = (el) => el.nodeName === 'SELECT'
-export const isNodeCheckbox = (el) =>
-  el.nodeName === 'INPUT' && el.type === 'checkbox'
+export const isNodeCheckbox = (el) => el.nodeName === 'INPUT' && el.type === 'checkbox'
 
 const updateSelectValue = (el, value) => {
   const optionToSelect = el.querySelector(`option[value="${value}"]`)
