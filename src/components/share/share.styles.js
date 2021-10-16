b43b5cbbe13e6a130104d17cb8416c12e16f5662
Diego Artiles <diego45882@hotmail.com>
2021-10-16 13:07:06 -0300

    feat(Root): add share component

---
+++ src/components/share/share.styles.js
@@ -0,0 +1,21 @@
+import { css } from 'lit'
+
+const ShareStyles = css`
+:host {
+
+}
+
+:host h4 {
+    text-align: center;
+}
+
+:host .share-options {
+    display: flex;
+}
+
+:host .share-options > div {
+    width: 50%;
+}
+`
+
+export default ShareStyles
