72acd9202a0de93744ad80a73526fbba892b5c21
David Brito <39559632+davbrito@users.noreply.github.com>
2022-01-01 14:57:40 -0400

    Use native EventTarget instead of mitt
    Replace the eventBus inplementation to use the native EventTarget API
    instead of a third party library.

--- src/events-controller.js
+++ src/events-controller.js
@@ -1,9 +1,24 @@
-import mitt from 'mitt'
 import { capitalize, searchByLine } from './utils/string.js'
 import { downloadUserCode } from './download.js'
 import { getState } from './state'
 
-export const eventBus = mitt()
+class EventBus extends window.EventTarget {
+  on (type, listener) {
+    this.addEventListener(type, listener)
+  }
+
+  off (type, listener) {
+    this.removeEventListener(type, listener)
+  }
+
+  emit (type, detail) {
+    const event = new window.CustomEvent(type, { detail, cancelable: true })
+
+    this.dispatchEvent(event)
+  }
+}
+
+export const eventBus = new EventBus()
 
 let jsEditor
 let htmlEditor
@@ -25,7 +40,7 @@ export const EVENTS = {
   DRAG_FILE: 'DRAG_FILE'
 }
 
-eventBus.on(EVENTS.ADD_SKYPACK_PACKAGE, ({ skypackPackage, url }) => {
+eventBus.on(EVENTS.ADD_SKYPACK_PACKAGE, ({ detail: { skypackPackage, url } }) => {
   const importStatement = `import ${capitalize(skypackPackage)} from '${url}';`
   const existPackage = searchByLine(jsEditor.getValue(), url)
   if (!existPackage) {
@@ -48,7 +63,7 @@ eventBus.on(EVENTS.DOWNLOAD_USER_CODE, () => {
   })
 })
 
-eventBus.on(EVENTS.DRAG_FILE, ({ content, typeFile }) => {
+eventBus.on(EVENTS.DRAG_FILE, ({ detail: { content, typeFile } }) => {
   const file = typeFile
 
   switch (file) {
