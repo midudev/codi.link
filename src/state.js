ab5b1c7b5ed6b8fe4bf1fb2d2202ffa193d817fb
David Brito <39559632+davbrito@users.noreply.github.com>
2022-09-29 20:29:26 -0400

    chore(Root): update zustand and use persist middleware

--- src/state.js
+++ src/state.js
@@ -1,33 +1,18 @@
+import { persist } from 'zustand/middleware'
 import create from 'zustand/vanilla'
 
 import { DEFAULT_INITIAL_SETTINGS } from './constants/initial-settings'
 
-const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key))
-const setLocalStorage = (key, value) =>
-  window.localStorage.setItem(key, JSON.stringify(value))
+const useStore = create(
+  persist(
+    (set, get) => ({
+      ...DEFAULT_INITIAL_SETTINGS,
+      updateSettings: ({ key, value }) => {
+        set({ [key]: value })
+      }
+    }),
+    { name: 'appInitialState', getStorage: () => window.localStorage }
+  )
+)
 
-const appInitialState = {
-  ...DEFAULT_INITIAL_SETTINGS,
-  ...getLocalStorage('appInitialState')
-}
-
-const useStore = create((set, get) => ({
-  ...appInitialState,
-  updateSettings: ({ key, value }) => {
-    set(state => {
-      setLocalStorage('appInitialState', {
-        ...state,
-        [key]: value
-      })
-
-      return { [key]: value }
-    })
-  }
-}))
-
-export const {
-  getState,
-  setState,
-  subscribe,
-  destroy
-} = useStore
+export const { getState, setState, subscribe, destroy } = useStore
