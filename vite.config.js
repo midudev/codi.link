c5433e921f92310fd87ee25c68d741b55ff33abe
maikCyphlock <maikolaguilar656@gmail.com>
2022-10-05 14:37:19 -0400

    fix error with top level await

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await' // support to old browser
 
 export default defineConfig({
   plugins: [
-    wasm()
+    wasm(),
+    topLevelAwait()
   ]
 })
