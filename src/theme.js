146676962dc198942736b41f11d14e7666946aee
Miguel Ángel Durán <miduga@gmail.com>
2023-12-25 13:02:44 +0100

    Export setTheme function in theme.js

--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,6 @@
  * @param {'vs-dark' | 'vs' | 'hc-black' | 'hc-light'} theme
  */
 
-const setTheme = theme => {
+export const setTheme = theme => {
   document.documentElement.setAttribute('data-theme', theme)
 }
-
-export default setTheme
