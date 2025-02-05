9d4bb46fce7fd7aa4bebbd1a6622ab6b69ccd950
midudev <miduga@gmail.com>
2025-02-05 11:16:28 +0100

    Migrate to tailwind 4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact(), tailwindcss()]
 })
