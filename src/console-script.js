c3a746a9fb89c477198b0206596d2932c8c6cc70
midudev <miduga@gmail.com>
2025-11-04 11:41:00 +0100

    feat: add comprehensive console API methods
    - Implemented console.count/countReset for tracking call frequencies with label support
    - Added console.time/timeEnd/timeLog for performance timing measurements
    - Included additional logging methods (debug, table, trace, dir, dirxml, assert, clear)

--- src/console-script.js
+++ src/console-script.js
@@ -20,6 +20,9 @@ export const generateConsoleScript = ({ html, css }) => {
         pushToConsole({line:fixedLine, column, message}, "error")
       }
 
+      const counts = {}
+      const timers = {}
+
       const console = {
         log: function(...args){
           pushToConsole(args, "log:log")
@@ -32,6 +35,53 @@ export const generateConsoleScript = ({ html, css }) => {
         },
         info: function(...args){
           pushToConsole(args, "log:info")
+        },
+        debug: function(...args){
+          pushToConsole(args, "log:debug")
+        },
+        table: function(data){
+          pushToConsole([data], "log:table")
+        },
+        count: function(label = 'default'){
+          counts[label] = (counts[label] || 0) + 1
+          pushToConsole([label + ": " + counts[label]], "log:count")
+        },
+        countReset: function(label = 'default'){
+          counts[label] = 0
+        },
+        trace: function(...args){
+          const stack = new Error().stack
+          pushToConsole([...args, stack], "log:trace")
+        },
+        dir: function(obj){
+          pushToConsole([obj], "log:dir")
+        },
+        dirxml: function(obj){
+          pushToConsole([obj], "log:dirxml")
+        },
+        time: function(label = 'default'){
+          timers[label] = performance.now()
+        },
+        timeEnd: function(label = 'default'){
+          if (timers[label]) {
+            const duration = performance.now() - timers[label]
+            pushToConsole([label + ": " + duration.toFixed(2) + "ms"], "log:time")
+            delete timers[label]
+          }
+        },
+        timeLog: function(label = 'default', ...args){
+          if (timers[label]) {
+            const duration = performance.now() - timers[label]
+            pushToConsole([label + ": " + duration.toFixed(2) + "ms"].concat(args), "log:time")
+          }
+        },
+        assert: function(condition, ...args){
+          if (!condition) {
+            pushToConsole(["Assertion failed:", ...args], "log:assert")
+          }
+        },
+        clear: function(){
+          pushToConsole("clear", "system")
         }
       }
 
